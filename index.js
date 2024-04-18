e999981634562a5f78c8a79f8d69c8fd9f7d0652
dylan <runnels.dylan@yahoo.com>
2024-04-18 13:24:13 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-function moveDodgerRight() {
-    let left = dodger.style.replace('px', '');
-   let left = parseInt(leftNumbers, 10);
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
   
-    if (left < gameWidth - dodgerWidth) {
-      dodger.style.left = `${left + 1}px`;
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
     }
   }
-  
-  document.addEventListener('keydown', function(e) {
-    if (e.key === "ArrowRight") {
+  document.addEventListener('keydown', function(event) {
+    if (event.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    else if (event.key === "ArrowRight") {
       moveDodgerRight();
     }
   });
+  function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10);
+    if (left < 360 && left + 40 < 400) {
+      dodger.style.left = `${left + 1}px`;
+        }
+    }
