8741ff87be19aa074070d406f368f4a35ff8a8e9
dylan <runnels.dylan@yahoo.com>
2024-04-18 13:15:08 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code here
+function moveDodgerRight() {
+    let left = dodger.style.replace('px', '');
+   let left = parseInt(leftNumbers, 10);
+  
+    if (left < gameWidth - dodgerWidth) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
+  document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
